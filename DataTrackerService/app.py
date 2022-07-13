be499816c632e9c06300c0ef84fab69bab9a4fe2
Krenil Bhikadiya <106650423+krenil-marutitech@users.noreply.github.com>
2022-07-13 17:17:41 +0530

    Updated app.py file.
    Removed unnecessary comments.

--- DataTrackerService/app.py
+++ DataTrackerService/app.py
@@ -16,43 +16,6 @@ app.config['JWT_SECRET_KEY'] = 'verysecret'
 jwt = JWTManager(app)
 api = Api(app)
 
-# esknn = ESKNN()
-# result = esknn.create_index()
-#
-#
-# @app.route('/api/add_user', methods=["POST"])
-# def add_user():
-#     data = flask.request.json
-#
-#     esknn.insert_user(data)
-#
-#     return jsonify(
-#         {
-#             "status": 200
-#         }
-#     )
-#
-#
-# @app.route('/api/search_user')
-# def search_user():
-#     data = flask.request.json
-#
-#     field_name = data['field_name']
-#     query = data['query']
-#
-#     results = esknn.search_user(query, field_name)
-#
-#     documents = []
-#
-#     hits = results['hits']['hits']
-#
-#     for hit in hits:
-#         documents.append(hit['_source'])
-#
-#     return {
-#         "status": 200,
-#         "documents": documents
-#     }
 api.add_resource(Users, '/api/user')
 api.add_resource(FetchMessages, '/api/search/<string:text>')
 api.add_resource(CountMessagesByCategory, '/api/msg_count_by_category/<string:category>')
